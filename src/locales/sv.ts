0b185f74426c3fc26fb13472bb4dd9e1e9773772
segelfartyg <samuel.swaren@outlook.com>
2024-02-10 00:26:25 +0100

    Add: init support swedish

---
+++ src/locales/sv.ts
@@ -0,0 +1,17 @@
+export const sv = {
+  feelingEmpty: "Känner mig lite tom...",
+  free: "Tomt",
+  loadNext: "Nästa",
+  loadPrevious: "Föregående",
+  over: "Över",
+  taken: "Bokat",
+  topbar: {
+    filters: "Filtrering",
+    next: "nästa",
+    prev: "föregående",
+    today: "IDAG",
+    view: "Zoom"
+  },
+  search: "Sök",
+  week: "Vecka"
+};
